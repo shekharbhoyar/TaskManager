63bc3e890d155abb7ae234c3c6531a563502882a
chandrashekhar <shekharbhoyar10@gmail.com>
2025-05-09 19:35:26 +0530

    user and tasks model added

---
+++ server/models/tasks.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  important: {
+    type: boolean,
+    default: false,
+  },
+  complete: {
+    type: boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("Tasks", taskSchema);
