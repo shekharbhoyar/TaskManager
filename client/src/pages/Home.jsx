e11b5f31066afa58eccf47b70c16a9f4ce673254
chandrashekhar <shekharbhoyar10@gmail.com>
2025-05-23 21:27:45 +0530

    menu

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
   return (
     <div className="flex  h-[98vh] gap-4">
       <div className=" w-1/6 border border-gray-500 rounded-xl p-4 flex flex-col justify-between">
-        <Sidebar />
+        <Sidebar /> //sidebar for menu
       </div>
       <div className=" w-5/6 border border-gray-500 rounded-xl p-4">
-        <Outlet />// we use this outlet component to render 
+        <Outlet />// we use this outlet component to render a component as a child
       </div>
     </div>
   );
