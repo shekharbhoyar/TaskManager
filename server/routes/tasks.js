39619d3f515f9d632ebc8192421725526794bc63
chandrashekhar <shekharbhoyar10@gmail.com>
2025-05-25 23:54:02 +0530

    taking tilte and tasks from body

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,6 +4,8 @@ const tasksroutes = Router();
 
 tasksroutes.post("/create-tasks", async (req, res) => {
   try {
+
+    const{title,desc}=req.body;
   } catch (err) {
     console.log(err);
     res.status(400).json({ massege: "internal server error" });
