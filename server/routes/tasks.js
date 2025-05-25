b2f57dada08bcfbc9a184fa305978ad087cc02ae
chandrashekhar <shekharbhoyar10@gmail.com>
2025-05-25 23:52:36 +0530

    create task route created

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,2 +1,11 @@
 import { Router } from "express";
 import tasks from "../models/tasks";
+const tasksroutes = Router();
+
+tasksroutes.post("/create-tasks", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ massege: "internal server error" });
+  }
+});
